576f8249595f59e1bc075a128ff30a6a74f8b36f
JibranH24 <jibranhussain2408@gmail.com>
2021-12-24 16:26:54 +0000

    added winner, line and changed background, buttons

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict"
 
 
-const colors = "green,red,blue,yellow,pink,silver,grey,gold,black,brown, ".split(",")
+
 
 let uniDivs=[]
 
@@ -19,7 +19,7 @@ while (i < numUnicorns) {
     uniDivs[i].style.left = 0 + "%"
 
     uniDivs[i].style.top = [i] * 5 + "%"
-    // uniDivs[i].style.backgroundColor = colors[i]
+    
     i++
 
     
@@ -36,32 +36,70 @@ while (j < numUnicorns) {
     j++
 }
     
-requestAnimationFrame(moveUnicorns)
+let winner=document.getElementById("winner")
 
-function moveUnicorns() {
+let startButton=document.getElementById("startButton")
+startButton.addEventListener("click",runTheGame)
 
-    let i = 0 
-    
-    while (uniXs[i] <= 80) {
-        uniXs[i] += Math.random()
-        uniDivs[i].style.left = uniXs[i] + "%"
 
-        if (uniXs[i] >= 80 ) {
-            alert(`Unicorn #${i} is the winner`)
-            resetGame()
+let playAgain=document.getElementById("playAgain")
+playAgain.addEventListener("click",resetHorses)
+
+
+function runTheGame(){
+    requestAnimationFrame(moveUnicorns)
+
+    function moveUnicorns() {
+
+        let i = 0 
+        
+        while (uniXs[i] <= 80) {
+            uniXs[i] += Math.random()*0.5
+            uniDivs[i].style.left = uniXs[i] + "%"
+
+            if (uniXs[i] >= 80 ) {
+                //alert(`Unicorn #${i+1} is the winner`)
+                winner.innerHTML=`Unicorn #${i+1} is the winner!`
+                winner.style.color="gold"
+                resetGame() //stops the game from making a new winner everytime a horse is above 80%
+                playAgain.style.height="40px"
+                playAgain.style.backgroundColor="gold"
+                startButton.style.backgroundColor="gray"
+            }
+
+            i++
         }
 
-        i++
+        
+        requestAnimationFrame(moveUnicorns)
     }
 
-    
-    requestAnimationFrame(moveUnicorns)
-}
-
-function resetGame() {
-    let k = 0
-    while (k < uniXs.length) {
-        uniXs[k] = 0 + "%"
-        k++
+    function resetGame() {
+        let k = 0
+        while (k < uniXs.length) {
+            uniXs[k] = 0 + "%"
+            k++
+        }
     }
 }
+
+function resetHorses(){
+    location.reload()
+}
+
+let finishLine=document.getElementById("finishLine")
+let ctx=finishLine.getContext("2d")
+
+let finishLineLength=finishLine.height/100*75
+let x=finishLine.width/100*80
+ctx.beginPath()
+ctx.moveTo(x,0)
+ctx.lineTo(x,finishLineLength)
+ctx.strokeStyle="red"
+ctx.stroke()
+
+
+
+
+
+
